bf69e2c227f182eb9f27ad5141ef7018a92d34c4
Nikita <rutina0grib@gmail.com>
2023-11-03 14:33:43 +0300

    added repositori

---
+++ src/components/employees-list-item/employees-list-item.js
@@ -0,0 +1,59 @@
+import { Component } from 'react';
+
+import './employees-list-item.css';
+
+class EmployeesListItem extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            increase: false,
+            promotion: false
+        }
+    }
+
+    onIncrease = () => {
+        this.setState(({ increase }) => ({
+            increase: !increase
+        }))
+    }
+
+    onPromotion = () => {
+        this.setState(({ promotion }) => ({
+            promotion: !promotion
+        }))
+    }
+
+    render() {
+        const { name, salary } = this.props;
+        const { increase, promotion } = this.state;
+
+        let classNames = 'list-group-item d-flex justify-content-between ';
+        if (increase) {
+            classNames += 'increase';
+        }
+        if (promotion) {
+            classNames += 'like'
+        }
+
+        return (
+            <li className={classNames}>
+                <span onClick={this.onPromotion} className="list-group-item-label">{name}</span>
+                <input type="text" className="list-group-item-input" defaultValue={salary + '$'} />
+                <div className='d-flex justify-content-center align-items-center'>
+                    <button onClick={this.onIncrease} type="button"
+                        className="btn-cookie btn-sm ">
+                        <i className="fas fa-cookie"></i>
+                    </button>
+
+                    <button type="button"
+                        className="btn-trash btn-sm ">
+                        <i className="fas fa-trash"></i>
+                    </button>
+                    <i className="fas fa-star"></i>
+                </div>
+            </li>
+        )
+    }
+}
+
+export default EmployeesListItem;
