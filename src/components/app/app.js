99e30162d46b7da4fd83b2980362d189721a590c
Nikita <rutina0grib@gmail.com>
2023-11-04 12:09:36 +0300

    finalization of the project implementation of specific items. adding employees, changing information about company employees and bonuses to be given, optimizing code.

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,3 +1,5 @@
+import { Component } from 'react';
+
 import AppInfo from '../app-info/app-info';
 import SearchPanel from '../search-panel/search-panel';
 import AppFilter from '../app-filter/app-filter';
@@ -6,27 +8,87 @@ import EmployeesAddForm from '../employees-add-form/employees-add-form';
 
 import './app.css';
 
-function App() {
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: [
+        { name: 'John C.', salary: 800, increase: false, promotion: false, id: 1 },
+        { name: 'Alex M.', salary: 3000, increase: false, promotion: false, id: 2 },
+        { name: 'Carl W.', salary: 5000, increase: false, promotion: false, id: 3 },
+      ],
+    }
+    this.maxId = 4;
+  }
 
-  const data = [
-    { name: 'John C.', salary: 800, increase: true, id: 1 },
-    { name: 'Alex M.', salary: 3000, increase: false, id: 2 },
-    { name: 'Carl W.', salary: 5000, increase: true, id: 3 },
-  ];
+  deleteEmployees = (id) => {
+    this.setState(({ data }) => ({
+      data: data.filter(item => item.id !== id),
+    }))
+  }
 
-  return (
-    <div className="app">
-      <AppInfo />
+  addEmployees = (name, salary) => {
+    const newCardsEmployees = {
+      name,
+      salary,
+      increase: false,
+      promotion: false,
+      id: this.maxId++
+    }
 
-      <div className="search-panel">
-        <SearchPanel />
-        <AppFilter />
+    this.setState(({ data }) => {
+      const newData = [...data, newCardsEmployees]
+      return {
+        data: newData
+      }
+    })
+  }
+
+  onToggleIncrease = (id) => {
+    this.setState(({ data }) => ({
+      data: data.map(item => {
+        if (item.id === id) {
+          return { ...item, increase: !item.increase }
+        }
+        return item;
+      })
+    }))
+  }
+
+  onTogglePromotion = (id) => {
+    this.setState(({ data }) => ({
+      data: data.map(item => {
+        if (item.id === id) {
+          return { ...item, promotion: !item.promotion }
+        }
+        return item;
+      })
+    }))
+  }
+
+  render() {
+    const counterEmployees = this.state.data.length;
+    const increaseEmployees = this.state.data.filter(item => item.increase).length;
+    return (
+      <div className="app">
+        <AppInfo
+          counterEmployees={counterEmployees}
+          increaseEmployees={increaseEmployees} />
+
+        <div className="search-panel">
+          <SearchPanel />
+          <AppFilter />
+        </div>
+
+        <EmployeesList
+          data={this.state.data}
+          onDelete={this.deleteEmployees}
+          onToggleIncrease={this.onToggleIncrease}
+          onTogglePromotion={this.onTogglePromotion} />
+        <EmployeesAddForm onAdd={this.addEmployees} />
       </div>
-
-      <EmployeesList data={data} />
-      <EmployeesAddForm />
-    </div>
-  );
+    )
+  }
 }
 
 export default App;
--- src/components/employees-list-item/employees-list-item.js
+++ src/components/employees-list-item/employees-list-item.js
@@ -1,59 +1,41 @@
-import { Component } from 'react';
-
 import './employees-list-item.css';
 
-class EmployeesListItem extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            increase: false,
-            promotion: false
-        }
+const EmployeesListItem = (props) => {
+    const { name,
+        salary,
+        onDelete,
+        onToggleIncrease,
+        onTogglePromotion,
+        increase,
+        promotion } = props;
+
+    let classNames = 'list-group-item d-flex justify-content-between ';
+    if (increase) {
+        classNames += 'increase';
+    }
+    if (promotion) {
+        classNames += 'like'
     }
 
-    onIncrease = () => {
-        this.setState(({ increase }) => ({
-            increase: !increase
-        }))
-    }
+    return (
+        <li className={classNames}>
+            <span onClick={onTogglePromotion} className="list-group-item-label">{name}</span>
+            <input type="text" className="list-group-item-input" defaultValue={salary + '$'} />
+            <div className='d-flex justify-content-center align-items-center'>
+                <button onClick={onToggleIncrease} type="button"
+                    className="btn-cookie btn-sm ">
+                    <i className="fas fa-cookie"></i>
+                </button>
 
-    onPromotion = () => {
-        this.setState(({ promotion }) => ({
-            promotion: !promotion
-        }))
-    }
-
-    render() {
-        const { name, salary } = this.props;
-        const { increase, promotion } = this.state;
-
-        let classNames = 'list-group-item d-flex justify-content-between ';
-        if (increase) {
-            classNames += 'increase';
-        }
-        if (promotion) {
-            classNames += 'like'
-        }
-
-        return (
-            <li className={classNames}>
-                <span onClick={this.onPromotion} className="list-group-item-label">{name}</span>
-                <input type="text" className="list-group-item-input" defaultValue={salary + '$'} />
-                <div className='d-flex justify-content-center align-items-center'>
-                    <button onClick={this.onIncrease} type="button"
-                        className="btn-cookie btn-sm ">
-                        <i className="fas fa-cookie"></i>
-                    </button>
-
-                    <button type="button"
-                        className="btn-trash btn-sm ">
-                        <i className="fas fa-trash"></i>
-                    </button>
-                    <i className="fas fa-star"></i>
-                </div>
-            </li>
-        )
-    }
+                <button type="button"
+                    className="btn-trash btn-sm "
+                    onClick={onDelete}>
+                    <i className="fas fa-trash"></i>
+                </button>
+                <i className="fas fa-star"></i>
+            </div>
+        </li>
+    )
 }
 
 export default EmployeesListItem;
